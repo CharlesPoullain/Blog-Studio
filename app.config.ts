d32bc581583f0134603c3b70ac3d999bca0c4e35
Charles <charles.poullain85@gmail.com>
2024-04-03 23:14:06 +0200

    Update colors and theme option

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,8 @@
 // https://github.com/nuxt-themes/alpine/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
-    description: 'The minimalist blog theme',
+    title: 'Charles ',
+    description: 'Charles Poullain Blog - Fullstack developer',
     image: {
       src: '/social-card-preview.png',
       alt: 'An image showcasing my project.',
@@ -10,25 +10,30 @@ export default defineAppConfig({
       height: 300
     },
     header: {
-      position: 'right', // possible value are : | 'left' | 'center' | 'right'
+      position: 'left', // possible value are : | 'left' | 'center' | 'right'
       logo: {
-        path: '/logo.svg', // path of the logo
-        pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
-        alt: 'alpine' // alt of the logo
+        path: '/logo.me.svg', // path of the logo
+        pathDark: '/logo.me.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
+        alt: 'Charles' // alt of the logo
       }
     },
     footer: {
       credits: {
-        enabled: true, // possible value are : true | false
-        repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
+        // possible value are : true | false
+        enabled: true,
+
+        // our github repository
+        repository: 'https://www.github.com/nuxt-themes/alpine',
+
+        text: 'Charles Poullain'
       },
       navigation: true, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
       message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
-      twitter: 'nuxtlabs',
-      instagram: 'atinuxt',
+      twitter: 'charlespoullain',
+      instagram: '',
       linkedin: {
         icon: 'uil:linkedin',
         label: 'LinkedIn',
